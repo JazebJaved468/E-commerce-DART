4c1c4ab597284365da5dac6ded8f47228011d476
Jazeb_Javed <jazebjaved52@gmail.com>
2023-07-18 23:31:06 +0500

    comments

--- index.dart
+++ index.dart
@@ -157,16 +157,20 @@ String toSentenceCase(String input) {
 }
 
 
-// COLORS AND FORMATTING
-// \u001b[1m: Sets the text style to bold.
-// \u001b[32m: Sets the text color to green.
-// \u001b[0m: Resets the text style and color to the default.
 
-// Black: \u001b[30m
-// Red: \u001b[31m
-// Green: \u001b[32m
-// Yellow: \u001b[33m
-// Blue: \u001b[34m
-// Magenta: \u001b[35m
-// Cyan: \u001b[36m
-// White: \u001b[37m
+/* FORMATTING
+
+\u001b[1m: Sets the text style to bold.
+\u001b[0m: Resets the text style and color to the default.
+
+COLORS
+Black: \u001b[30m
+Red: \u001b[31m
+Green: \u001b[32m
+Yellow: \u001b[33m
+Blue: \u001b[34m
+Magenta: \u001b[35m
+Cyan: \u001b[36m
+White: \u001b[37m
+
+*/
